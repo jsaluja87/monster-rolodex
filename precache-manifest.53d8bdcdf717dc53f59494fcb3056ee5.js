f606dcaeeed4f66e6a54b12ca2c521ddeec8f7df
jsaluja87 <saluja.jaspreetsingh@gmail.com>
2020-10-14 21:47:37 -0700

    Updates

---
+++ precache-manifest.53d8bdcdf717dc53f59494fcb3056ee5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d83f7c28df4ec9118bf21c29e4500c0d",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "c83d12aab3e1b1d0b188",
+    "url": "/monster-rolodex/static/css/main.3f0da9b0.chunk.css"
+  },
+  {
+    "revision": "8961004a6b589eb17a4c",
+    "url": "/monster-rolodex/static/js/2.e1a0f1bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.e1a0f1bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c83d12aab3e1b1d0b188",
+    "url": "/monster-rolodex/static/js/main.0a967639.chunk.js"
+  },
+  {
+    "revision": "3d5dc42248b87f1c09c5",
+    "url": "/monster-rolodex/static/js/runtime-main.22ce2adb.js"
+  }
+]);
